4d261f712f7fb66ae6df87c66e94495fc729ce15
phillip-che <pche@cpp.edu>
2024-04-22 21:49:48 -0700

    refactor code and extract api repsonse

--- chatbot.py
+++ chatbot.py
@@ -3,22 +3,28 @@ from dotenv import load_dotenv
 import os
 
 class Chatbot:
+
   def __init__(self):
     load_dotenv()
+    self.messages = []
     self.client = OpenAI(
       api_key = os.getenv('api_key')
     )
+    
+  def get_response(self, message):
+    self.messages.append({"role":"user", "content":message})
+    response = chatbot.client.chat.completions.create(
+      model = "gpt-3.5-turbo",
+      messages = self.messages
+    )
+    reply = response.choices[0].message.content
+    self.messages.append({"role":"system", "content":reply})
+
+    return reply
 
 if __name__ == "__main__":
   chatbot = Chatbot()
-  response = chatbot.client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {
-        "role": "user",
-        "content": "Tell me a joke"
-      }
-    ],
-  )
+  bot_type = input("What type of chatbot would you like to create?\n")
+  chatbot.messages.append({"role": "system", "content": bot_type})
 
-  print(response.choices[0].message.content)
+  print(chatbot.get_response(input("Ask a question: ")))
